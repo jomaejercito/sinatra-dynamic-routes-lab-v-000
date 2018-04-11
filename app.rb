055f0d8b380b01d5b616a79dbaa372066aff5cd9
Joma Ejercito <jomaejercito@gmail.com>
2018-04-11 03:29:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+  get "/reversename/:name" do
+    @name = params[:name]
+    "#{@name.reverse}"
+  end
+
+  get "/multiply/:num1/:num2" do
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
+    "#{@num1 * @num2}"
+  end
+
+  get "/goodbye/:name" do
+    @user_name = params[:name]
+    "Goodbye, #{@user_name}."
+  end
+
 
 end
