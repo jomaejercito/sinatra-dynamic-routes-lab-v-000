eb971ff5b0de7678bac58cb6e96c23cf9e6169c5
Joma Ejercito <jomaejercito@gmail.com>
2018-04-11 03:33:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,15 +2,18 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+
   get "/reversename/:name" do
     @name = params[:name]
     "#{@name.reverse}"
   end
 
+
   get "/square/:number" do
     @number = params[:number].to_i
     "#{@number * @number}"
   end
+
 
   get "/say/:number/:phrase" do
     @number = params[:number].to_i
@@ -23,14 +26,17 @@ class App < Sinatra::Base
     @phrase_array
   end
 
+
   get "/say/:word1/:word2/:word3/:word4/:word5" do
     @word1 = params[:word1]
     @word2 = params[:word2]
     @word3 = params[:word3]
     @word4 = params[:word4]
     @word5 = params[:word5]
+
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
+
 
   get "/:operation/:number1/:number2" do
     @operation = params[:operation]
@@ -46,6 +52,7 @@ class App < Sinatra::Base
     elsif  @operation == "divide"
       "#{@number1 / @number2}"
     end
+    
   end
 
 
